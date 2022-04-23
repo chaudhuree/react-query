ac72bfafa84b31ff120f5a0f15d46de00445d19e
chaudhuree <chaudhuree00@gmail.com>
2022-04-23 22:54:04 +0600

    docs:quickstart with query provider ,query client and devtool

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  QueryClient,
+  QueryClientProvider
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
 import './index.css';
-
+const client = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
 
