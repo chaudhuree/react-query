391796ebea856eed4d4230d3c73e6e730e713226
chaudhuree <chaudhuree00@gmail.com>
2022-04-24 00:11:19 +0600

    docs:useQuery()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useQuery } from 'react-query';
 import './App.css';
-
 function App() {
+  // const {data,error}=useQuery("hello",()=>{
+  //   return Promise.resolve(4)
+  // })
+  // console.log(data,error);
+  const {data:data2,isError,isLoading,isSuccess}=useQuery("hello2",()=>{
+    return new Promise((resolve)=>{
+      setTimeout(()=>{
+        resolve(5)
+      },1000)
+    }
+    )
+  })
+  console.log({data2,isError,isLoading,isSuccess});
   return (
     <div className="App">
       <h2>chaudhuree</h2>
