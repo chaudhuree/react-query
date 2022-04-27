0b246390fe800e7cf701c0c8bd111c16b7320ce1
chaudhuree <chaudhuree00@gmail.com>
2022-04-27 21:50:14 +0600

    docs: react state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import React from 'react';
 import { useQuery } from 'react-query';
 import './App.css';
-function App() {
-  // const {data,error}=useQuery("hello",()=>{
-  //   return Promise.resolve(4)
-  // })
-  // console.log(data,error);
+const Button=()=>{
   const {data:data2,isError,isLoading,isSuccess}=useQuery("hello2",()=>{
     return new Promise((resolve)=>{
       setTimeout(()=>{
-        resolve(5)
+        resolve(Math.random())
       },1000)
     }
     )
   })
   console.log({data2,isError,isLoading,isSuccess});
+  return <button>i am a button {data2}</button>
+}
+function App() {
+  const [visible,setVisible]=React.useState(true);
+  // const {data,error}=useQuery("hello",()=>{
+  //   return Promise.resolve(4)
+  // })
+  // console.log(data,error);
+const toggleButton=()=>setVisible(!visible)
   return (
     <div className="App">
       <h2>chaudhuree</h2>
+      {visible && <Button/>}
+      <br />
+      <button onClick={toggleButton}>toggle </button>
     </div>
   );
 }
